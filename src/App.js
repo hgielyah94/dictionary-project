da99559842329b5acfa8003c012f9480f36f152f
Hayleigh Winpenny <hayleigh.winpenny@outlook.com>
2021-08-03 00:46:02 +0100

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <h6>Dictionary App<br /><small id="description">SheCodes React Project</small></h6>
     <Dictionary />
     <h6 className="footer"><small>Coded by </small><a href="/">Hayleigh</a> 👩‍💻</h6>
+    <h6><a href="https://github.com/hgielyah94/dictionary-project" target="_blank" rel="noreferrer">View on GitHub</a></h6>
     </div>
   );
 }
