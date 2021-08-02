3762889f0152ba27d3184306d46136332169088d
Hayleigh Winpenny <hayleigh.winpenny@outlook.com>
2021-08-02 11:30:48 +0100

    Synonyms capitalised

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,18 +1,16 @@
 import React from "react";
 
 export default function Synonyms(props) {
-    if (props.synonyms) {
-        return (
-            <ul className="Synonyms">
-            {props.synonyms.map(function (synonym, index) {
-                return
-                    <li key={index}>
-                        {synonym}
-                    </li>;
-            })}
-            </ul>
-        );
-    } else {
-        return null;
-    }
+  if (props.synonyms) {
+    return (
+      <span className="Synonyms">
+        <p id="Synonyms" className="text-capitalize"><strong>Synonyms:</strong></p>
+        {props.synonyms.map(function (synonym, index) {
+          return <p id="Synonyms" className="text-capitalize" key={index}>{synonym}, </p>;
+        })}
+      </span>
+    );
+  } else {
+    return null;
+  }
 }
